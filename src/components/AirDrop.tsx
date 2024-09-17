02c0e11262b42002e755159f97e1b62ae66cffec
nihar-hegde <niharhegde163@gmail.com>
2024-09-17 20:38:27 +0530

    adding input

--- src/components/AirDrop.tsx
+++ src/components/AirDrop.tsx
@@ -1,15 +1,35 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { ChangeEvent, useState } from "react";
 
 export const AirDrop = () => {
+  const [amount, setAmount] = useState<number>(0);
   const wallet = useWallet();
   const { connection } = useConnection();
 
   const sendAirDropToUser = async () => {
     if (wallet.publicKey) {
-      await connection.requestAirdrop(wallet.publicKey, 2000000000);
+      try {
+        const signature = await connection.requestAirdrop(
+          wallet.publicKey,
+          amount,
+        );
+        await connection.confirmTransaction(signature);
+        console.log(`AirDrop of ${amount / 1e9} SOL successfull!`);
+      } catch (error) {
+        console.log(error);
+      }
     } else {
-      console.log("Error");
+      console.log("Error! Wallet not connected.");
     }
+  };
+
+  const handleAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+
+    const solAmount = parseFloat(inputValue) || 0;
+
+    const lamports = Math.floor(solAmount * 1e9);
+    setAmount(lamports);
   };
 
   return (
@@ -18,9 +38,13 @@ export const AirDrop = () => {
       <div className="flex flex-col gap-2 items-center">
         <p>{wallet.publicKey?.toString()}</p>
         <input
-          type="text"
-          placeholder="Amount"
+          type="number"
+          placeholder="Amount in SOL"
+          value={amount / 1e9} // Display in SOL
+          onChange={handleAmountChange}
           className="border bg-gray-300 p-2 rounded-md"
+          min="0"
+          step="0.000000001"
         />
         <button
           className="bg-gray-300 rounded-md p-2"
